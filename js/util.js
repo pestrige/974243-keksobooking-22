4b00a15c2bc56ff9dab36bfce161591a9c06f815
pestrige <pestrige@gmail.com>
2021-03-12 10:56:03 +0300

    Разбил проект на модули

---
+++ js/util.js
@@ -0,0 +1,30 @@
+// Генерируем случайное целое число
+
+const generateInt = (a, b) => {
+  const min = Math.ceil(Math.min(a, b));
+  const max = Math.floor(Math.max(a, b));
+
+  if (min < 0) {
+    return NaN;
+  }
+
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+// Генерируем число с плаваюшей точкой
+
+const generateFloat = (a, b, digits) => {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+
+  if (min < 0) {
+    throw new Error('Отрицательное значение недопустимо');
+  }
+
+  const exponent = Math.pow(10, digits);
+  const random = min + Math.random() * (max - min);
+
+  return (Math.trunc(random * exponent) / exponent).toFixed(digits); //toFixed для отображения нулей
+};
+
+export { generateInt, generateFloat };
